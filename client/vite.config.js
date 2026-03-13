36b70781a7cdc95eab182d7029c98a9966a10c35
addmininfoservice-ux <syam1245@gmail.com>
2026-03-13 18:17:53 +0530

    render error fix

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,9 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { VitePWA } from 'vite-plugin-pwa'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      'chip-level-track-shared': path.resolve(__dirname, '../shared')
+    }
+  },
   plugins: [
     react(),
     VitePWA({
